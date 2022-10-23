f5f9734e39a48516e2d0458633061ef7a666e2c4
RD <>
2022-10-23 16:32:11 +1100

    Create project

---
+++ LiquorShop/LiquorShopApp.swift
@@ -0,0 +1,17 @@
+//
+//  LiquorShopApp.swift
+//  LiquorShop
+//
+//  Created by RD on 23/10/22.
+//
+
+import SwiftUI
+
+@main
+struct LiquorShopApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
